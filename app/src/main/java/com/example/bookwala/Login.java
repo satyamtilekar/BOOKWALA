9e53b4487c4a990dc0336286b51d6e2e22ed8948
Nikhilrautatgithub <nikhilraut.nr7@gmail.com>
2020-07-05 23:06:55 +0530

    second commit . login firebase

--- app/src/main/java/com/example/bookwala/Login.java
+++ app/src/main/java/com/example/bookwala/Login.java
@@ -1,14 +1,93 @@
 package com.example.bookwala;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+    private EditText log_email , log_password ;
+    private Button login_button;
+    private TextView signUp;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth=FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+
+        mDialog=new ProgressDialog(this);
+
+        log_email=findViewById(R.id.login_email);
+        log_password=findViewById(R.id.login_pass);
+
+        login_button=findViewById(R.id.login_button);
+        signUp=findViewById(R.id.Register_link);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mEmail=log_email.getText().toString().trim();
+                String mPass=log_password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(mEmail)){
+                    email.setError("Required Field..");
+                    return;
+                }
+                if (TextUtils.isEmpty(mPass)){
+                    pass.setError("Required Field..");
+                    return;
+                }
+
+                mDialog.setMessage("Processing..");
+                mDialog.show();
+
+                mAuth.signInWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+
+                            mDialog.dismiss();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+
+                        }
+                    }
+                });
+            }
+        });
+            signUp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(),Register.class));
+                }
+            });
+
+
     }
 }
--- app/src/main/java/com/example/bookwala/Register.java
+++ app/src/main/java/com/example/bookwala/Register.java
@@ -2,13 +2,30 @@ package com.example.bookwala;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+    private EditText first_name,last_name,reg_mail,reg_password;
+    private Button reg_button;
 
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth=FirebaseAuth.getInstance();
+        mDialog=new ProgressDialog(this);
+
+        first_name=findViewById(R.id.reg_firstname);
+        last_name=
     }
 }
