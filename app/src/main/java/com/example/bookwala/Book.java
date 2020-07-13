b47c3dd1d75615853b14377f3ac12f9cd6a196d7
satyamtilekar <satyamtilekar0001@gmail.com>
2020-07-13 17:42:20 +0530

    bottom navigation view OP wala

---
+++ app/src/main/java/com/example/bookwala/Book.java
@@ -0,0 +1,54 @@
+package com.example.bookwala;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Book extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+
+        //Initialization and Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.Book);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            private MenuItem item;
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.Book:
+                        return true;
+                    case R.id.Add:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Add.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.Chat:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Chat.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.Profile:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Profile.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
