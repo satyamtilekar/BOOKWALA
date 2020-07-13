6efa5b139ed10f2d14e7e724577a98e4da535015
satyamtilekar <satyamtilekar0001@gmail.com>
2020-07-13 17:41:19 +0530

    bottom navigation view OP wala

--- app/src/main/java/com/example/bookwala/MainActivity.java
+++ app/src/main/java/com/example/bookwala/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.bookwala;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialization and Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.Book);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            private MenuItem item;
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.Book:
+                        return true;
+                    case R.id.Add:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Add.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.Chat:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Chat.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.Profile:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Profile.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
 
